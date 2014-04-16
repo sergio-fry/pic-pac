c848f2af94fcf92ade710254d4bc088e49ea0a8f
Sergei O. Udalov <sergei.udalov@gmail.com>
2014-04-16 18:23:27 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PicPac::Application.config.secret_key_base = '1a6aa1a527d061ab9dc2ca3e77b7c3265340863b92a64d018943d6e00357f9215d00eb08f86b0a18463336ad9e8c704374ba292b2503e9d79c08ca9b4d3e4e2f'
