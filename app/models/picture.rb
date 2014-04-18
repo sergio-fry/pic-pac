6988977718df3033d79b2fdd4c3000f06e986fe4
Sergei O. Udalov <sergei.udalov@gmail.com>
2014-04-18 15:58:03 +0400

    erase file after destroy

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,6 +3,8 @@ require 'open-uri'
 require 'digest/sha1'
 
 class Picture < ActiveRecord::Base
+  before_destroy :erase_file
+
   def resize(width, height=nil)
     width, height = width.to_i, height.try(:to_i)
     img = Magick::Image.read(src_url)[0]
@@ -40,5 +42,10 @@ class Picture < ActiveRecord::Base
 
   def hash
     Digest::SHA1.hexdigest(id.to_s + PicPac::Application.config.secret_key_base)[0..5]
+  end
+
+  def erase_file
+    bucket = AWS_STORE.directories.get ENV['S3_BUCKET_NAME']
+    bucket.files.create(:key => "resized/#{id}/#{hash}.jpeg", :body => "removed", :public => true)
   end
 end
