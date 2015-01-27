5a6a9383e8d8e4e8ba4feb9e25c85e057efeec0e
Sergei O. Udalov <sergei.udalov@gmail.com>
2015-01-27 12:34:22 +0300

    do not retry failed

--- app/workers/cleaner.rb
+++ app/workers/cleaner.rb
@@ -1,5 +1,6 @@
 class Cleaner
   include Sidekiq::Worker
+  sidekiq_options :retry => false
 
   def perform(picture_id)
     Picture.find(picture_id).destroy
--- app/workers/resizer.rb
+++ app/workers/resizer.rb
@@ -1,6 +1,6 @@
 class Resizer
   include Sidekiq::Worker
-  sidekiq_options :queue => :high_priority
+  sidekiq_options :queue => :high_priority, :retry => false
 
   def perform(picture_id, width, height)
     Picture.find(picture_id).resize(width, height)
